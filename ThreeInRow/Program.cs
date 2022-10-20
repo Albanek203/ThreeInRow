6117209ee190238a091a08a6e7da68f1aad1a904
Albanek203 <oleglovchuk@ukr.net>
2022-10-20 20:41:30 +0300

    initial commit

---
+++ ThreeInRow/Program.cs
@@ -0,0 +1,21 @@
+﻿using ThreeInRow.models;
+
+var gameTile = new GameTile(9, 9);
+var field = gameTile.GetField();
+
+Console.WriteLine("Game tile before:");
+PrintMatrix(field);
+
+var changeCount = 0;
+while (gameTile.CheckCoincide()) { changeCount++; }
+
+Console.WriteLine("Game tile after:");
+PrintMatrix(field);
+Console.WriteLine($"Changed {changeCount} times");
+
+void PrintMatrix(List<List<int>> matrix) {
+    foreach (var row in matrix) {
+        foreach (var element in row) { Console.Write(element + " "); }
+        Console.WriteLine();
+    }
+}
---
+++ ThreeInRow/models/GameTile.cs
@@ -0,0 +1,91 @@
+﻿namespace ThreeInRow.models;
+
+public class GameTile {
+    private readonly int _rowCount;
+    private readonly int _columnCount;
+    private List<List<int>> Field { get; set; }
+
+    public GameTile(int rows, int columns) {
+        _rowCount = rows;
+        _columnCount = columns;
+        Field = new List<List<int>>();
+        for (var i = 0; i < columns; i++) {
+            Field.Add(new List<int>());
+            for (var j = 0; j < rows; j++) { Field[i].Add(0); }
+        }
+        CreateNewField();
+    }
+
+    public void CreateNewField() {
+        foreach (var row in Field) {
+            for (var j = 0; j < row.Count; j++) { row[j] = GetRandomNumber(); }
+        }
+    }
+
+    public bool CheckCoincide() {
+        var hasCoincide = false;
+
+        // Check horizontal coincide
+        for (var i = 0; i < _columnCount; i++) {
+            for (var j = 0; j < _rowCount; j++) {
+                var k = 1;
+                var s = 1;
+                while (j + s < _rowCount) {
+                    if (Field[i][j] == Field[i][j + s]) {
+                        s++;
+                        k++;
+                    }
+                    else { break; }
+                }
+                if (k < 3) continue;
+                ChangeRow(i, j, k);
+                hasCoincide = true;
+            }
+        }
+
+        // Check vertical coincide
+        for (var i = 0; i < _rowCount; i++) {
+            for (var j = 0; j < _columnCount; j++) {
+                var k = 1;
+                var s = 1;
+                while (j + s < _columnCount) {
+                    if (Field[j][i] == Field[j + s][i]) {
+                        s++;
+                        k++;
+                    }
+                    else { break; }
+                }
+                if (k < 3) continue;
+                ChangeColumn(j, i, k);
+                hasCoincide = true;
+            }
+        }
+        return hasCoincide;
+    }
+
+    private void ChangeRow(int startPointRow, int startPointColumn, int count) {
+        if (startPointRow == 0) {
+            for (var i = 0; i < count; i++, startPointColumn++)
+                Field[startPointRow][startPointColumn] = GetRandomNumber();
+        }
+        else {
+            for (var i = 0; i < count; i++, startPointColumn++) {
+                Field[startPointRow][startPointColumn] = Field[startPointRow - 1][startPointColumn];
+                Field[startPointRow - 1][startPointColumn] = GetRandomNumber();
+            }
+        }
+    }
+
+    private void ChangeColumn(int startPointRow, int startPointColumn, int count) {
+        var endPointRow = startPointRow + count - 1;
+        while (endPointRow >= 0) {
+            if (endPointRow - count < 0) { Field[endPointRow][startPointColumn] = GetRandomNumber(); }
+            else { Field[endPointRow][startPointColumn] = Field[endPointRow - count][startPointColumn]; }
+            endPointRow--;
+        }
+    }
+
+    private int GetRandomNumber() => new Random().Next(0, 4);
+
+    public List<List<int>> GetField() => Field;
+}
